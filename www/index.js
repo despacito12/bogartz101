929f70122b597d97066c9f790a03bc0943130971
despacito12 <31024960+despacito12@users.noreply.github.com>
2017-10-28 00:27:04 +0800

    Add files via upload

---
+++ www/index.js
@@ -0,0 +1,41 @@
+(function(){
+  
+  var list = document.querySelector('#list'),
+      form = document.querySelector('form'),
+      item = document.querySelector('#item');
+  
+  form.addEventListener('submit',function(e){
+    e.preventDefault();
+    list.innerHTML += '<li>' + item.value + '</li>';
+    store();
+    item.value = "";
+  },false)
+  
+  list.addEventListener('click',function(e){
+    var t = e.target;
+    if(t.classList.contains('checked')){
+      t.parentNode.removeChild(t);
+    } else {
+      t.classList.add('checked');
+    }
+    store();
+  },false)
+  
+  function store() {
+    window.localStorage.myitems = list.innerHTML;
+  }
+  
+  function getValues() {
+    var storedValues = window.localStorage.myitems;
+    if(!storedValues) {
+      list.innerHTML = '<li>Eat kikiam</li>'+
+                       '<li>Go to School</li>'+
+                       '<li>Play LOL</li>'+
+                       '<li>Reward yourself with a nap</li>';
+    }
+    else {
+      list.innerHTML = storedValues;
+    }
+  }
+  getValues();
+})();
